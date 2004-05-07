e679108e466da40101d2a87f946cb0afdff594c1
Timothy Corbett-Clark <tcorbettclark@@users.sf.net>
2004-05-07 10:35:48 +0000

    fixed bug with setting up paths

--- bin/setup-paths.sh
+++ bin/setup-paths.sh
@@ -4,9 +4,11 @@
 
 # Source this file when running midge commands before installation.
 
-pushd $(dirname $0) >/dev/null
-
-export PYTHONPATH=${PYTHONPATH}:$(pwd)/..
-export PATH=${PATH}:$(pwd)
-
-popd >/dev/null
+if (ls | grep setup-paths.sh >/dev/null); then
+    pushd $(dirname $0) >/dev/null
+    export PYTHONPATH=${PYTHONPATH}:$(pwd)/..
+    export PATH=${PATH}:$(pwd)
+    popd >/dev/null
+else
+    echo "Please source this file from within its directory."
+fi
